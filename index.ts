80e43e8ae92725cc50b9fc5fed09c83a0bf820c0
Rig-ved <rick.bhattacharya@gmail.com>
2021-11-09 12:24:06 +0530

    2 files modified

--- index.ts
+++ index.ts
@@ -2,8 +2,8 @@
 import './style.css';
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+const appDiv: HTMLElement = document.querySelector('#app');
+appDiv.innerHTML = `<h1>Is it yes or is it no</h1>`;
 
 //
 
